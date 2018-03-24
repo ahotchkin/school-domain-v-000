57743d3783bfecf12b90063e41a2147c1fc8f0ae
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-03-24 19:25:56 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School
+  attr_reader :school_name, :roster
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    unless @roster.include?(grade)
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+end
